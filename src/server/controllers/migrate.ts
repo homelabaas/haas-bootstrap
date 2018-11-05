7639c667114cadc26931dc40161eb20380ddbf9e
Geoffrey Rich <geoff.rich@gmail.com>
2018-11-05 11:03:15 +1000

    Add pg test.

--- src/server/controllers/migrate.ts
+++ src/server/controllers/migrate.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import { Client } from "pg";
 import { IConnectResponse } from "../../common/models/IConnectResponse";
 import { IMigrateRequest } from "../../common/models/IMigrateRequest";
 
@@ -7,7 +8,15 @@ const router: Router = Router();
 router.post("/", async (req: Request, res: Response) => {
     const migrateRequest = req.body as IMigrateRequest;
     try {
-        //
+        const pgClient = new Client({
+            user: migrateRequest.user,
+            host: migrateRequest.address,
+            database: migrateRequest.db,
+            password: migrateRequest.password,
+            port: migrateRequest.port,
+          });
+        await pgClient.connect();
+        const result = await pgClient.query("SELECT * FROM VM");
         const connectResponse: IConnectResponse = {
             Success: true,
             Message: "Successfully connected."
